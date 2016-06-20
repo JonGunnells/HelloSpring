bfbeafcccb4674aad5d26acd0d36713663612c9f
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-20 12:09:06 -0400

    Added Login/Logout functionality

--- src/main/java/com/theironyard/HelloSpringController.java
+++ src/main/java/com/theironyard/HelloSpringController.java
@@ -6,10 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HelloSpringController {
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model, String name, String city, Integer age) {
+    public String home(Model model, HttpSession session, String name, String city, Integer age) {
         if (name == null){
             name = "Alice";
     }
@@ -22,9 +25,29 @@ public class HelloSpringController {
 
         Person p = new Person(name, city, age);
         model.addAttribute("person", p);
+
+        String username = (String) session.getAttribute("username");
+        User user = null; //create initial object
+        if (username !=null) {
+            user = new User(username);
+        }
+        model.addAttribute("user", user);
         return "person"; //html document name
 
 
+    }
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String username, HttpSession session) {
+        session.setAttribute("username", username);
+
+        return "redirect:/";
+
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 
 
