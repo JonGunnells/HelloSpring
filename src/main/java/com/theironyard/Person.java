25a27c6dbcac1104f8f8a48a033327d7e8b8c09e
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-20 11:42:37 -0400

    Created Project

---
+++ src/main/java/com/theironyard/Person.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+
+public class Person {
+    String name;
+    String city;
+    Integer age;
+
+    public Person(String name, String city, Integer age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+}
